85ccf34b8a1aec232d1a3fac0224d2da2db6728c
dkbozkurt <dkaanbozkurt@gmail.com>
2023-01-29 16:38:27 +0300

    Character Movement with Joystick

---
+++ Assets/Game/Scripts/Behaviours/CubeStackAndFollow/CollectibleCubeBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Game.Scripts.Behaviours.CubeStackAndFollow
+{
+    public class CollectibleCubeBehaviour : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Behaviours/CubeStackAndFollow/PlayerStackingBehaviour.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Game.Scripts.Behaviours.CubeStackAndFollow
+{
+    public class PlayerStackingBehaviour : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+        
+        void Start()
+        {
+                    
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
