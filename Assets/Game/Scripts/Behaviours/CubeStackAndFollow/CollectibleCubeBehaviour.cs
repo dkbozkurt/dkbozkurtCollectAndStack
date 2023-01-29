978fac58d98fe491b4695394fa9c7abd634c8df1
dkbozkurt <dkaanbozkurt@gmail.com>
2023-01-29 16:54:04 +0300

    Stacked objects lerp logic added
    https://github.com/MuhammetFatihYilmaz/Cube-Stacking-And-Following-Project/tree/master/Assets/Scripts

--- Assets/Game/Scripts/Behaviours/CubeStackAndFollow/CollectibleCubeBehaviour.cs
+++ Assets/Game/Scripts/Behaviours/CubeStackAndFollow/CollectibleCubeBehaviour.cs
@@ -1,19 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Scripts.Behaviours.CubeStackAndFollow
 {
     public class CollectibleCubeBehaviour : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private float _followSpeed;
+
+        public void UpdateCubePosition(Transform followedCube, bool isFollowStart)
         {
-        
+            StartCoroutine(StartFollowingToLastCubePosition(followedCube, isFollowStart));
         }
 
-        // Update is called once per frame
-        void Update()
+        IEnumerator StartFollowingToLastCubePosition(Transform followedCube, bool isFollowStart)
         {
-        
+
+            while (isFollowStart)
+            {
+                yield return new WaitForEndOfFrame();
+                transform.position = new Vector3(Mathf.Lerp(transform.position.x, followedCube.position.x, _followSpeed * Time.deltaTime),
+                    transform.position.y,
+                    Mathf.Lerp(transform.position.z, followedCube.position.z, _followSpeed * Time.deltaTime));
+            }
         }
     }
 }
